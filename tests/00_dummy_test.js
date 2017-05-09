c079cb48ca5b2c03e1112dd9035e741fa7f5ec79
Andraž Dobnikar <andraz.dobnikar@signapps.si>
2017-05-09 13:07:30 +0200

    Example dummy test template.

--- tests/00_dummy_test.js
+++ tests/00_dummy_test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const assert = require('assert');
-require('../libs/seedrandom-ext');
 
 exports.setUp = async function() {
   // Prepare the data for the test. This is an optional function.
@@ -11,6 +10,7 @@ exports.setUp = async function() {
 exports.test = async function() {
   // Run the test. This function is required.
   // ...
+
 };
 
 exports.tearDown = async function() {
