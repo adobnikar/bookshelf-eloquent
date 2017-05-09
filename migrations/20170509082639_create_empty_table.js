b29c974a9fa1f4b3f6ecf71d5931b7fe9597ce69
Andraž Dobnikar <andraz.dobnikar@signapps.si>
2017-05-09 13:05:03 +0200

    Empty table migration.

---
+++ migrations/20170509082639_create_empty_table.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.up = async function(knex, Promise) {
+  await knex.schema.createTable('empty', function(table) {
+    table.charset('utf8');
+    table.collate('utf8_unicode_ci');
+
+    table.increments('id').unsigned().primary();
+    table.string('name', 64).notNullable().unique();
+    table.integer('number').notNullable();
+
+    // Timestamps.
+    table.dateTime('createdAt').notNullable().defaultTo(knex.fn.now()).index();
+    table.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = async function(knex, Promise) {
+  await knex.schema.dropTable('empty');
+};
