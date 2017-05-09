bb181d1670da01128ab62a87ec2ab1191191401a
Andraž Dobnikar <andraz.dobnikar@signapps.si>
2017-05-09 13:07:56 +0200

    With has many test.

---
+++ tests/03_with_has_many_test.js
@@ -0,0 +1,169 @@
+'use strict';
+
+const connection = require('../knexfile');
+const knex = require('knex')(connection);
+
+const assert = require('assert');
+require('../libs/seedrandom-ext');
+
+const isArray = require('lodash/isArray');
+const union = require('lodash/union');
+
+const Comment = require('../models/comment');
+const Enrolment = require('../models/enrolment');
+const Friend = require('../models/friend');
+const Group = require('../models/group');
+const Post = require('../models/post');
+const Rating = require('../models/rating');
+const Role = require('../models/role');
+const Tag = require('../models/tag');
+const User = require('../models/user');
+const Empty = require('../models/empty');
+
+function modelAttrs(model) {
+  return model.attributes;
+}
+
+function bookify(Model) {
+  return function(obj) {
+    obj.__proto__ = null;
+
+    // Remove hidden columns.
+    if (Model != null) {
+      if (Model.prototype.hidden != null) {
+        for (let attr of Model.prototype.hidden) {
+          delete obj[attr];
+        }
+      }
+    }
+
+    return obj;
+  };
+}
+
+function removeProto(obj) {
+  obj.__proto__ = null;
+
+  for (let property in obj) {
+    if (isArray(obj[property]))
+      obj[property] = obj[property].map(removeProto);
+  }
+
+  return obj;
+}
+
+exports.test = async function() {
+  // Run the test. This function is required.
+  // List of all models for easier testing.
+  let models = [Comment, Enrolment, Friend, Group, Post, Rating, Role, Tag,
+    User, Empty];
+  let softDeleteModels = [Comment, Group, Post, User];
+  let nonSoftDeleteModels = [Enrolment, Friend, Rating, Role, Tag, Empty];
+  let knexResult = null;
+  let bookResult = null;
+
+  knexResult = (await knex.select().from(User.prototype.tableName)
+    .whereNull('deletedAt')).map(bookify(User));
+  let knexResultPost = new Map();
+  (await knex.select().from(Post.prototype.tableName)
+    .whereNull('deletedAt')).map(bookify(Post)).map((e) => {
+      if (!knexResultPost.has(e.createdById))
+        knexResultPost.set(e.createdById, []);
+      knexResultPost.get(e.createdById).push(e);
+    });
+  let knexResultComments = new Map();
+  (await knex.select().from(Comment.prototype.tableName)
+    .whereNull('deletedAt')).map(bookify(Comment)).map((e) => {
+      if (!knexResultComments.has(e.postId))
+        knexResultComments.set(e.postId, []);
+      knexResultComments.get(e.postId).push(e);
+    });
+
+  for (let user of knexResult) {
+    if (knexResultPost.has(user.id)) user.posts = knexResultPost.get(user.id);
+    else user.posts = [];
+    for (let post of user.posts) {
+      if (knexResultComments.has(post.id))
+        post.comments = knexResultComments.get(post.id);
+      else post.comments = [];
+    }
+  }
+
+  bookResult = (await User.with('posts.comments').get())
+    .toJSON().map(removeProto);
+  assert.deepStrictEqual(knexResult, bookResult);
+
+  // With select.
+  knexResult = (await knex.select().from(User.prototype.tableName)
+    .whereNull('deletedAt')).map(bookify(User));
+  knexResultPost = new Map();
+  (await knex.select().from(Post.prototype.tableName)
+    .whereNull('deletedAt')).map(bookify(Post)).map((e) => {
+      if (!knexResultPost.has(e.createdById))
+        knexResultPost.set(e.createdById, []);
+      knexResultPost.get(e.createdById).push(e);
+    });
+  knexResultComments = new Map();
+  (await knex.select(union(['text', 'postId'],
+    [Comment.prototype.idAttribute])).from(Comment.prototype.tableName)
+    .whereNull('deletedAt')).map(bookify(Comment)).map((e) => {
+      if (!knexResultComments.has(e.postId))
+        knexResultComments.set(e.postId, []);
+      knexResultComments.get(e.postId).push(e);
+    });
+
+  for (let user of knexResult) {
+    if (knexResultPost.has(user.id)) user.posts = knexResultPost.get(user.id);
+    else user.posts = [];
+    for (let post of user.posts) {
+      if (knexResultComments.has(post.id))
+        post.comments = knexResultComments.get(post.id);
+      else post.comments = [];
+    }
+  }
+
+  bookResult = (await User.withSelect('posts.comments',
+    ['text']).get())
+    .toJSON().map(removeProto);
+  assert.deepStrictEqual(knexResult, bookResult);
+
+  // Nested with.
+  knexResult = (await knex.select().from(User.prototype.tableName)
+    .whereNull('deletedAt')).map(bookify(User));
+  knexResultPost = new Map();
+  (await knex.select(['id', 'text', 'createdById'])
+    .from(Post.prototype.tableName)
+    .whereNull('deletedAt')
+    .where('title', 'not like', 'a%'))
+    .map(bookify(Post)).map((e) => {
+      if (!knexResultPost.has(e.createdById))
+        knexResultPost.set(e.createdById, []);
+      knexResultPost.get(e.createdById).push(e);
+    });
+  knexResultComments = new Map();
+  (await knex.select(union(['text', 'postId'],
+    [Comment.prototype.idAttribute])).from(Comment.prototype.tableName)
+    .whereNull('deletedAt')).map(bookify(Comment)).map((e) => {
+      if (!knexResultComments.has(e.postId))
+        knexResultComments.set(e.postId, []);
+      knexResultComments.get(e.postId).push(e);
+    });
+
+  for (let user of knexResult) {
+    if (knexResultPost.has(user.id)) user.posts = knexResultPost.get(user.id);
+    else user.posts = [];
+    for (let post of user.posts) {
+      if (knexResultComments.has(post.id))
+        post.comments = knexResultComments.get(post.id);
+      else post.comments = [];
+    }
+  }
+
+  bookResult = (await User.withSelect('posts', [], (q) => {
+    q.select('text');
+    q.whereNotLike('title', 'a%');
+    q.withSelect('comments', 'text');
+  }).get())
+    .toJSON().map(removeProto);
+  assert.deepStrictEqual(knexResult, bookResult);
+};
